ad5b2f329023675f50ece7954048427cb0e3414f
naysok <ysok.ysok@gmail.com>
2020-04-08 03:59:23 +0900

    [add] uitls

---
+++ py/operate_color/util.py
@@ -0,0 +1,32 @@
+#########################################################
+###                                                   ###
+###                    Utilities                      ###
+###                                                   ###
+#########################################################
+
+
+import math
+import random
+
+
+class UTIL():
+
+
+    def remap_number(self, src, old_min, old_max, new_min, new_max):
+        return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+
+
+    def ll_size(self, list_list):
+        return len(list_list), len(list_list[0])
+    
+
+    def set_vector(self, res):
+        vecs = []
+        for i in range(res):
+            for j in range(res):
+                vecs.append((i, j))
+        return vecs
+
+
+    def pt2d_add(self, pt, vec):
+        return (pt[0] + vec[0], pt[1] + vec[1])
